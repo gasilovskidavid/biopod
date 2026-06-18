2c8621fe5ecefb12aae11c7679ed1c078c9510bb
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-18 22:11:03 +0200

    feat(simulator): add dynamic simulation for sensor reading, turn into permanent stream to Kinesis

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,9 +1,11 @@
 import json
 import os
-
 import boto3
 from dotenv import load_dotenv
-from dataclasses import dataclass
+from dataclasses import dataclass, asdict
+import time
+import math
+import random
 
 load_dotenv()
 
@@ -22,10 +24,28 @@ class SensorReading:
     water_ph: float
     timestamp: str
 
-response = client.put_record(
-    StreamName=STREAM_NAME,
-    Data=json.dumps(record).encode("utf-8"),
-    PartitionKey="alpha",
-)
+t = 0
 
-print(response)
+while True :
+
+    reading = SensorReading(
+    pod_id = "alpha",
+    co2_ppm = 800 + 200 * math.sin(t) + random.gauss(0,10),
+    temperature_c = 24 + 3 * math.sin(t) + random.gauss(0,0.2),
+    ppf_umols = 400 + 400 * math.sin(t) + random.gauss(0,5),
+    rh_pct = 70 + 10 * math.sin(t) + random.gauss(0,1),
+    water_ph = 6.5 + 0.5 *  math.sin(t) + random.gauss(0,0.05),
+    timestamp = time.asctime()
+    )
+
+    response = client.put_record(
+        StreamName=STREAM_NAME,
+        Data=json.dumps(asdict(reading)).encode("utf-8"),
+        PartitionKey=reading.pod_id,
+    )
+
+    t+=1
+
+    time.sleep(5)
+
+    print(f"Sent: t={t}, co2={reading.co2_ppm:.1f}, temperature_c={reading.temperature_c:.1f}, ppf={reading.ppf_umols:.1f}, relative_humidity={reading.rh_pct:.1f}, water_ph={reading.water_ph:.1f}")
