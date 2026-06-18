74a6059b0ae58219483c9c814707aeda8ba2d2bc
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-18 22:52:51 +0200

    chore(simulator): change names to values, pass the lint test

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,11 +1,12 @@
 import json
+import math
 import os
+import random
+import time
+from dataclasses import asdict, dataclass
+
 import boto3
 from dotenv import load_dotenv
-from dataclasses import dataclass, asdict
-import time
-import math
-import random
 
 load_dotenv()
 
@@ -19,7 +20,7 @@ class SensorReading:
     pod_id: str
     co2_ppm: float
     temperature_c: float
-    ppf_umols: float
+    light_ppf: float
     rh_pct: float
     water_ph: float
     timestamp: str
@@ -32,7 +33,7 @@ while True :
     pod_id = "alpha",
     co2_ppm = 800 + 200 * math.sin(t) + random.gauss(0,10),
     temperature_c = 24 + 3 * math.sin(t) + random.gauss(0,0.2),
-    ppf_umols = 400 + 400 * math.sin(t) + random.gauss(0,5),
+    light_ppfd = 400 + 400 * math.sin(t) + random.gauss(0,5),
     rh_pct = 70 + 10 * math.sin(t) + random.gauss(0,1),
     water_ph = 6.5 + 0.5 *  math.sin(t) + random.gauss(0,0.05),
     timestamp = time.asctime()
@@ -48,4 +49,10 @@ while True :
 
     time.sleep(5)
 
-    print(f"Sent: t={t}, co2={reading.co2_ppm:.1f}, temperature_c={reading.temperature_c:.1f}, ppf={reading.ppf_umols:.1f}, relative_humidity={reading.rh_pct:.1f}, water_ph={reading.water_ph:.1f}")
+    print(
+        f"Sent: t={t}, co2={reading.co2_ppm:.1f}, "
+        f"temperature_c={reading.temperature_c:.1f}, "
+        f"ppfd={reading.light_ppfd:.1f}, "
+        f"relative_humidity={reading.rh_pct:.1f}, "
+        f"water_ph={reading.water_ph:.1f}"
+        )
