e8a1db712c8898b8fc881ebcd2243029df31b606
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-18 17:43:23 +0200

    chore: add Ruff linting for pyhton with newly linted code

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,26 +1,23 @@
-import json 
-import boto3
+import json
 import os
+
+import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
 
-STREAM_NAME = os.environ.get('STREAM_NAME')
-AWS_REGION = os.environ.get('AWS_REGION')
+STREAM_NAME = os.environ.get("STREAM_NAME")
+AWS_REGION = os.environ.get("AWS_REGION")
 
-client = boto3.client('kinesis', region_name=AWS_REGION)
+client = boto3.client("kinesis", region_name=AWS_REGION)
 
-record = {
-    "pod_id" : "alpha",
-    "co2_ppm" : 800, 
-    "temperature_c" : 22.5
-}
+record = {"pod_id": "alpha", "co2_ppm": 800, "temperature_c": 22.5}
 
 
 response = client.put_record(
-    StreamName = STREAM_NAME,
-    Data = json.dumps(record).encode('utf-8'),
-    PartitionKey = "alpha"
+    StreamName=STREAM_NAME,
+    Data=json.dumps(record).encode("utf-8"),
+    PartitionKey="alpha",
 )
 
 print(response)
