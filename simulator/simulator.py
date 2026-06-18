d4d73dac889be931d00cbe328a29fa43ec1d6fe0
David Gasilovskii <78368566+gasilovskidavid@users.noreply.github.com>
2026-06-18 23:07:50 +0200

    chore: adress missed changes for last PR (#4.1)
    * chore: add Ruff linting for pyhton with newly linted code
    
    * chore: add yaml code for CI in GitHub Actions
    
    * fix(ci): correct folder name
    
    * chore(ci): comment out the Terraform part, because there are no terraform files in the repo for now
    
    * fix(ci): add "install dependancies" block
    
    * feat: create class for sensor reading
    
    * feat(simulator): add dynamic simulation for sensor reading, turn into permanent stream to Kinesis
    
    * chore(simulator): change names to values, pass the lint test
    
    * fix(simulator): correct name for dataclass variable
    
    * chore(simulator): pass the unfixable lint check
    
    * chore(simulator): ruff format the file

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -1,12 +1,12 @@
 import json
+import math
 import os
+import random
+import time
+from dataclasses import asdict, dataclass
 
 import boto3
 from dotenv import load_dotenv
-from dataclasses import dataclass, asdict
-import time
-import math
-import random
 
 load_dotenv()
 
@@ -15,28 +15,29 @@ AWS_REGION = os.environ.get("AWS_REGION")
 
 client = boto3.client("kinesis", region_name=AWS_REGION)
 
+
 @dataclass
 class SensorReading:
     pod_id: str
     co2_ppm: float
     temperature_c: float
-    ppf_umols: float
+    light_ppfd: float
     rh_pct: float
     water_ph: float
     timestamp: str
 
+
 t = 0
 
-while True :
-
+while True:
     reading = SensorReading(
-    pod_id = "alpha",
-    co2_ppm = 800 + 200 * math.sin(t) + random.gauss(0,10),
-    temperature_c = 24 + 3 * math.sin(t) + random.gauss(0,0.2),
-    ppf_umols = 400 + 400 * math.sin(t) + random.gauss(0,5),
-    rh_pct = 70 + 10 * math.sin(t) + random.gauss(0,1),
-    water_ph = 6.5 + 0.5 *  math.sin(t) + random.gauss(0,0.05),
-    timestamp = time.asctime()
+        pod_id="alpha",
+        co2_ppm=800 + 200 * math.sin(t) + random.gauss(0, 10),
+        temperature_c=24 + 3 * math.sin(t) + random.gauss(0, 0.2),
+        light_ppfd=400 + 400 * math.sin(t) + random.gauss(0, 5),
+        rh_pct=70 + 10 * math.sin(t) + random.gauss(0, 1),
+        water_ph=6.5 + 0.5 * math.sin(t) + random.gauss(0, 0.05),
+        timestamp=time.asctime(),
     )
 
     response = client.put_record(
@@ -45,8 +46,14 @@ while True :
         PartitionKey=reading.pod_id,
     )
 
-    t+=1
+    t += 1
 
     time.sleep(5)
 
-    print(f"Sent: t={t}, co2={reading.co2_ppm:.1f}, temperature_c={reading.temperature_c:.1f}, ppf={reading.ppf_umols:.1f}, relative_humidity={reading.rh_pct:.1f}, water_ph={reading.water_ph:.1f}")
+    print(
+        f"Sent: t={t}, co2={reading.co2_ppm:.1f}, "
+        f"temperature_c={reading.temperature_c:.1f}, "
+        f"ppfd={reading.light_ppfd:.1f}, "
+        f"relative_humidity={reading.rh_pct:.1f}, "
+        f"water_ph={reading.water_ph:.1f}"
+    )
