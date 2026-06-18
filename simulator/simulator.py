a4668fca4d9838c1d7833b342b6e09a787a1857b
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-18 19:37:09 +0200

    feat: create class for sensor reading

--- simulator/simulator.py
+++ simulator/simulator.py
@@ -3,6 +3,7 @@ import os
 
 import boto3
 from dotenv import load_dotenv
+from dataclasses import dataclass
 
 load_dotenv()
 
@@ -11,8 +12,15 @@ AWS_REGION = os.environ.get("AWS_REGION")
 
 client = boto3.client("kinesis", region_name=AWS_REGION)
 
-record = {"pod_id": "alpha", "co2_ppm": 800, "temperature_c": 22.5}
-
+@dataclass
+class SensorReading:
+    pod_id: str
+    co2_ppm: float
+    temperature_c: float
+    ppf_umols: float
+    rh_pct: float
+    water_ph: float
+    timestamp: str
 
 response = client.put_record(
     StreamName=STREAM_NAME,
