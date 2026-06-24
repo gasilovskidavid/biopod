2a3773ef16215f633e02ee52e8c662807d40fd61
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-24 19:34:13 +0200

    chore(infrastructure/main): add existing kinesis stream to terraform

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -10,3 +10,13 @@ terraform {
 provider "aws" {
   region = "eu-west-3"
 }
+
+resource "aws_kinesis_stream" "biopod-telemetry" {
+  name  = "biopod-telemetry"
+  shard_count = 1
+  retention_period = 24
+
+  tags = {
+    Project = "biopod"
+  }
+}
