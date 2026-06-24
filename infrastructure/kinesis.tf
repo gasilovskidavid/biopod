147af1ed03f37d2325e591cd45b36883dd818982
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-24 19:59:00 +0200

    refacto(infrastructure): reorganise main.tf into more clean multi-file structure

--- infrastructure/kinesis.tf
+++ infrastructure/kinesis.tf
@@ -1,16 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-west-3"
-}
-
 resource "aws_kinesis_stream" "biopod-telemetry" {
   name             = "biopod-telemetry"
   shard_count      = 1
