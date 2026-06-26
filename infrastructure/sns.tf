0d65cf28a69529d0fec513dc4e435e6128d01789
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-26 22:10:25 +0200

    fix(sns): protocol "email" instead of "sqs" :-\

--- infrastructure/sns.tf
+++ infrastructure/sns.tf
@@ -4,6 +4,6 @@ resource "aws_sns_topic" "co2_alerts" {
 
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
   topic_arn = aws_sns_topic.co2_alerts.arn
-  protocol  = "sqs"
+  protocol  = "email"
   endpoint  = var.alert_mail
 }
