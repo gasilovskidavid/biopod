1b0aa80e9b09f8f9db7600ad82a4bc8d52c42032
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-26 20:53:19 +0200

    fix(iam): run terraform fmt for previous commit

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -54,7 +54,7 @@ data "aws_iam_policy_document" "biopod_consumer_policy" {
   }
 
   statement {
-    sid = "SNSAlerting"
+    sid    = "SNSAlerting"
     effect = "Allow"
     actions = [
       "sns:Publish"
