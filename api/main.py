ea14a1e8b4eb6e3c118ec9eb3df873e11ad39af5
David Gasilovskii <gasilovski.david@gmail.com>
2026-06-30 21:18:46 +0200

    chore(api): ruff fix for previous commit

--- api/main.py
+++ api/main.py
@@ -1,16 +1,17 @@
-from fastapi import FastAPI, HTTPException
-from pydantic import BaseModel
-from boto3.dynamodb.types import TypeDeserializer
+import os
 
 import boto3
-import os
+from boto3.dynamodb.types import TypeDeserializer
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 AWS_REGION = os.environ.get("AWS_REGION")
 DYNAMODB_TABLE_NAME = os.environ.get("DYNAMODB_TABLE_NAME")
 
-dynamodb_client = boto3.client('dynamodb', region_name=AWS_REGION)
+dynamodb_client = boto3.client("dynamodb", region_name=AWS_REGION)
 
 app = FastAPI()
+
 
 class Item(BaseModel):
     pod_id: str
@@ -20,6 +21,7 @@ class Item(BaseModel):
     rh_pct: float
     water_ph: float
     light_ppfd: float
+
 
 @app.get("/items/{pod_id}/{start_time}/{end_time}", response_model=list[Item])
 def querydb(pod_id: str, start_time: int, end_time: int) -> list[Item]:
@@ -34,11 +36,17 @@ def querydb(pod_id: str, start_time: int, end_time: int) -> list[Item]:
         },
     )
     if not response["Items"]:
-        raise HTTPException(status_code=404, detail=f"No items found for pod {pod_id}"
-                            f"for selected timerange")
+        raise HTTPException(
+            status_code=404,
+            detail=f"No items found for pod {pod_id}for selected timerange",
+        )
     deserializer = TypeDeserializer()
     return [
-        {k: str(deserializer.deserialize(v)) if k == "timestamp" else deserializer.deserialize(v)
-         for k, v in item.items()}
+        {
+            k: str(deserializer.deserialize(v))
+            if k == "timestamp"
+            else deserializer.deserialize(v)
+            for k, v in item.items()
+        }
         for item in response["Items"]
     ]
