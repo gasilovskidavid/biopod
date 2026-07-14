5ac9046dfc0d90bdfc86658b1f4aba6eb264d247
David Gasilovskii <gasilovski.david@gmail.com>
2026-07-14 13:26:37 +0200

    fix(network): fix double referencing for ci/cd

--- infrastructure/key_pair.tf
+++ infrastructure/key_pair.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "admin" {
   key_name   = "biopod-admin-key"
-  public_key = file(pathexpand("~/.ssh/id_ed25519.pub"))
+  public_key = var.ssh_public_key
 }
--- infrastructure/network.tf
+++ infrastructure/network.tf
@@ -24,7 +24,3 @@ data "aws_ami" "al2023_arm64" {
   }
 }
 
-resource "aws_key_pair" "admin" {
-  key_name   = "biopod-admin-key"
-  public_key = var.ssh_public_key
-}
